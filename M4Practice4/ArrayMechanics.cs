d8870cdc89d7f0caea532136ef5b098ecab2da5a
Anton Lugom <mogul086@mail.ru>
2022-11-26 20:14:59 +0300

    initial

---
+++ M4Practice4/ArrayMechanics.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M4Practice4
+{
+    /// <summary>
+    /// Предоставляет функционал для работы с матрицами
+    /// </summary>
+    internal class ArrayMechanics
+    {
+        /*
+         * Данный метод не относится к работе с матрицами и по-хорошему его 
+         * необходимо выделять в отдельный класс
+         */
+        /// <summary>
+        /// Проверяет корректность ввода целого числа и возвращает его значение
+        /// </summary>
+        /// <returns>Целочисленный результат пользовательского ввода</returns>
+        public static int InputNumberCheck()
+        {
+            bool userInputCheck;
+            int correctInput;
+
+            do
+            {
+                Console.WriteLine("Введите целое число: ");
+                string userInput = Console.ReadLine().ToString();
+                userInputCheck = int.TryParse(userInput, out correctInput);
+
+            } while (!userInputCheck);
+
+            return correctInput;
+        }
+
+        /// <summary>
+        /// Создание и наполнение матрицы целыми числами
+        /// </summary>
+        /// <param name="rowNumbers">Количество строк</param>
+        /// <param name="colNumbers">Количество столбцов</param>
+        /// <returns>Возвращает матрицу заполненную целыми числами в диапазоне от -100 до 100</returns>
+        public static int[,] Array2DGenerator(int rowNumbers, int colNumbers)
+        {
+            // Random random = new Random(); // VS 2022 предложил более простую конструкцию ниже
+            Random random = new();
+            int[,] array = new int[rowNumbers, colNumbers];
+
+            for (int rowIndex = 0; rowIndex < rowNumbers; rowIndex++)
+            {
+                for (int colIndex = 0; colIndex < colNumbers; colIndex++)
+                {
+                    array[rowIndex, colIndex] = random.Next(-100, 101);
+                }
+            }
+
+            return array;
+        }
+
+        /// <summary>
+        /// Выводит в консоль значения матрицы
+        /// </summary>
+        /// <param name="arrayToDraw">Матрица для отображения</param>
+        public static void DrawArray(int[,] arrayToDraw)
+        {
+
+            for (int rowIndex = 0; rowIndex < arrayToDraw.GetLength(0); rowIndex++)
+            {
+                Console.WriteLine();
+                for (int colIndex = 0; colIndex < arrayToDraw.GetLength(1); colIndex++)
+                {
+                    Console.Write($"{arrayToDraw[rowIndex, colIndex],4}");
+                }
+
+            }
+        }
+
+        /// <summary>
+        /// Выводит в консоль значения матрицы и возвращает сумму элементов
+        /// </summary>
+        /// <param name="arrayToDraw">Матрица для отображения</param>
+        /// <param name="sum">Сумма всех элементов матрицы</param>
+        public static void DrawArray(int[,] arrayToDraw, out int sum)
+        {
+            sum = 0;
+
+            for (int rowIndex = 0; rowIndex < arrayToDraw.GetLength(0); rowIndex++)
+            {
+                Console.WriteLine();
+                for (int colIndex = 0; colIndex < arrayToDraw.GetLength(1); colIndex++)
+                {
+                    Console.Write($"{arrayToDraw[rowIndex, colIndex],4}");
+                    sum += arrayToDraw[rowIndex, colIndex];
+                }
+
+            }
+        }
+
+    }
+}
---
+++ M4Practice4/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace M4Practice4
+{
+    /// <summary>
+    /// Практическое задание 4. Работа с двумерными массивами (матрица)
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// Точка старта
+        /// </summary>
+        static void Main()
+        {
+            Console.WriteLine("Практическое работа 4.");
+            // Задание 1, создание матрицы и  вывод в консоль значений элементов, а также суммы всех элементов
+            Console.WriteLine("Задание 1.Создание случайной матрицы");
+            Console.WriteLine("Введите количество строк в матрице");
+            int rowsArray = ArrayMechanics.InputNumberCheck();
+            Console.WriteLine("Введите количество столбцов в матрице");
+            int colArray = ArrayMechanics.InputNumberCheck();
+
+            int[,] firstArray = ArrayMechanics.Array2DGenerator(rowsArray, colArray);
+
+            // рисуем матрицу
+            ArrayMechanics.DrawArray(firstArray, out int sumElementsArray);
+
+            // вывод суммы элементов
+            Console.WriteLine($"\nСумма элементов матрицы: {sumElementsArray}");
+            Console.ReadKey(true);
+            Console.Clear();
+
+            // Задание 2 сложение двух матриц
+            Console.WriteLine("Практическое работа 4.");
+            Console.WriteLine("Задание 2.Сложение матриц");
+            Console.WriteLine("Используем размерность и матрицу из первого задания");
+
+            int[,] secondArray = ArrayMechanics.Array2DGenerator(rowsArray, colArray);
+
+            //рисуем матрицы
+            Console.WriteLine("\nПервая матрица");
+            ArrayMechanics.DrawArray(firstArray);
+            Console.WriteLine("\n\nВторая матрица");
+            ArrayMechanics.DrawArray(secondArray);
+            Console.WriteLine("\n\nРезультирующая матрица:");
+
+            // вывод на экран результат сложения матриц
+            for (int rowsIndex = 0; rowsIndex < rowsArray; rowsIndex++)
+            {
+                Console.WriteLine();
+                for (int colIndex = 0; colIndex < colArray; colIndex++)
+                {
+                    int twoArraySumElements = firstArray[rowsIndex, colIndex] + secondArray[rowsIndex, colIndex];
+                    Console.Write($"{twoArraySumElements, 4}");
+                }
+
+            }
+            Console.ReadKey(true);
+
+        }
+
+        
+    }
+}
